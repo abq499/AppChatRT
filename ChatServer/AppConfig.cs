b38ae2d2c1ddbdbe8509b9e26288fa617252df95
abq499 <doduythinh07012005@gmail.com>
2026-06-22 11:56:56 +0700

    Update

--- ChatServer/AppConfig.cs
+++ ChatServer/AppConfig.cs
@@ -3,7 +3,7 @@
     public static class AppConfig
     {
         public static string ConnectionString =
-            @"Server=LAPTOP-TKFEE1EB\XMSSQLSERVER;Database=ChatAppDB;Trusted_Connection=True;TrustServerCertificate=True;";
+            @"Server=YOUR_SERVER_NAME;Database=ChatAppDB;Trusted_Connection=True;TrustServerCertificate=True;";
 
         public static string LoadBalancerIp = "127.0.0.1";
         public static int LoadBalancerPort = 8800;
